5f41a94db4bb2bb6a587209d5ba52f47b3e0ffbd
Edward FitzGerald <edward.fitzgerald@fetch.ai>
2019-05-26 19:59:09 +0100

    Update version to v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cairn',
-    version='0.1.0',
+    version='0.2.0',
     description='Tools and utilities for managing project versions',
     url='https://github.com/ejfitzgerald/cairn/',
     author='Ed FitzGerald',
